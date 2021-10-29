1908cff82b11819ed19a64862d49a2dbcf153986
qoxogus <a01034367055@gmail.com>
2021-10-30 02:58:20 +0900

    [ADD] access denied, entry point 예외처리

---
+++ src/main/java/com/example/demo/exception_advice/basic_error/BasicErrorHandler.java
@@ -0,0 +1,2 @@
+package com.example.demo.exception_advice.basic_error;public class BasicErrorHandler {
+}
---
+++ src/main/java/com/example/demo/exception_advice/response/CustomException.java
@@ -0,0 +1,2 @@
+package com.example.demo.exception_advice.response;public class CustomException {
+}

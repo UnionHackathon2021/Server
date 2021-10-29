d4f963b790664bbf7be52bc8cc5e05111cbbf31b
qoxogus <a01034367055@gmail.com>
2021-10-30 02:59:03 +0900

    [ADD] object util

--- src/main/java/com/example/demo/util/ExceptionResponseObjectUtil.java
+++ src/main/java/com/example/demo/util/ExceptionResponseObjectUtil.java
@@ -1,2 +1,25 @@
-package com.example.demo.util;public class ExceptionResponseObjectUtil {
+package com.example.demo.util;
+
+import com.example.demo.response.ResponseService;
+import com.example.demo.response.result.CommonResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExceptionResponseObjectUtil {
+
+    private final ResponseService responseService;
+    private final MessageSource messageSource;
+
+    private String getMessage(String code){
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    }
+
+    public CommonResult getExceptionResponseObj(String code){
+        return responseService.getFailResult(Integer.parseInt(getMessage(code + ".code")), getMessage(code + ".msg"));
+    }
+
 }

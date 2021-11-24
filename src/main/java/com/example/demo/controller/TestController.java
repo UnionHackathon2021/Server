ac39f9a75419788df3b52ebff4cde1da8a71fd92
qoxogus <a01034367055@gmail.com>
2021-11-24 10:04:54 +0900

    [TEST] test

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -12,6 +12,6 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
 //        throw new MemberNotFoundException();
-        return "Hello World!";
+        return "Test";
     }
 }
